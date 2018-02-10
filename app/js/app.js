cf0918ec854744f183a21bfa5a370a1665b80b26
themeplify <futurefocus2k17@gmail.com>
2018-02-10 17:55:51 +0530

    index updated

--- app/js/app.js
+++ app/js/app.js
@@ -77,3 +77,21 @@
 //         }
 //     );
 // });
+
+$(document).ready(function(){
+  $grid = $('.list').isotope({
+    // options
+    itemSelector: '.list__item',
+    layoutMode: 'masonry',
+    masonry: {
+      gutter: 0
+    }
+  });
+  // filter items on button click
+  $('.js-filter').on( 'click', 'button', function() {
+    var filterValue = $(this).attr('data-filter');
+    $grid.isotope({ filter: filterValue });
+    $( '.js-filter button' ).removeClass( 'is-active' );
+    $( this ).addClass( 'is-active' );
+  });
+});
